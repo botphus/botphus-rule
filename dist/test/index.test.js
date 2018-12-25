4080b653cb52df59a50ecf77fb0f658cd8b2d6cc
bakajinsei <cqggff@gmail.com>
2018-12-25 18:23:41 +0800

    [*]Change coverage

--- dist/test/index.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const rule_1 = require("./rule/");
-describe('Parse', () => {
-    // Test Task
-    rule_1.default();
-});
